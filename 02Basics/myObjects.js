4aebec37110db2c4b1eac85b1b8d9ccf049d040e
harsh <focusedstudytarget2022@gmail.com>
2024-11-10 01:13:03 +0530

    type alias

--- 02Basics/myObjects.js
+++ 02Basics/myObjects.js
@@ -1,21 +1,18 @@
 "use strict";
-// const User = {
-//   name: "harsh",
-//   email: "harsh@gmail.com",
-//   isPaid:false
-// };
 Object.defineProperty(exports, "__esModule", { value: true });
-// function createUser({ name: string, isPaid: boolean }) {}
-// createUser({ name: "harsh", isPaid: false });
-// //weird(bad) behaviour
-// // createUser({ name: "harsh", isPaid: false, email:"" }); //not allowed
-// //but this is allowed
-// createUser(User);
-// function createCourse(): { name: string; price: number } {
-//   return { name: "reactjs", price: 399 };
-// }
-// export {};
+var User = {
+    name: "harsh",
+    email: "harsh@gmail.com",
+    isPaid: false,
+};
 function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
+    var name = _a.name, isPaid = _a.isPaid;
 }
-createUser({ name: 2, isPaid: false });
+createUser({ name: "harsh", isPaid: false });
+//weird(bad) behaviour
+// createUser({ name: "harsh", isPaid: false, email:"" }); //not allowed
+//but this is allowed
+createUser(User);
+function createCourse() {
+    return { name: "reactjs", price: 399 };
+}
