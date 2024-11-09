52bb18e297a1572d97fd0569c2a9a4919d073f5d
harsh <focusedstudytarget2022@gmail.com>
2024-11-10 01:34:13 +0530

    Arrays

---
+++ 02Basics/myArray.ts
@@ -0,0 +1,23 @@
+const superHeroes: string[] = [];
+
+const heroPower: Array<number> = [];
+// const heroPower: number[] = [];
+// both are same
+
+superHeroes.push("thor", "spiderman", "ironman");
+heroPower.push(2);
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUser: User[] = [];
+allUser.push({ name: "rj", isActive: true });
+
+const MLModels: number[][] = [
+  [255, 255, 255],
+  [10, 13],
+];
+
+export {};
